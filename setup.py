2de444a08e23563ca900db1969838dc29795b041
David Stocker <david.stocker@sap.com>
2016-09-19 16:19:25 -0700

    Added setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,19 @@
+'''
+Created on September 19, 2016
+
+@author: David Stocker
+'''
+
+from distutils.core import setup
+setup(
+  name = 'memetic',
+  packages = ['memetic'], 
+  version = '1.0',
+  description = 'An open source property graph schema and graph traverse language.',
+  author = 'David Stocker',
+  author_email = 'mrdave991@gmail.com',
+  url = 'https://github.com/davidhstocker/Memetic', 
+  download_url = 'https://github.com/davidhstocker/Memetic/tarball/1.0', 
+  keywords = ['graph', 'propertygraph', 'property graph', 'graphdatabase', 'graph database', 'schema'],
+  classifiers = [],
+)
