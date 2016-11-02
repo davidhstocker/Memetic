eb97d095f92bf83f04da90bed22c52c14427a992
David Stocker <david.stocker@sap.com>
2016-11-02 07:47:54 +0100

    Minor tweak to SES and normalized the setup.py version and tag version

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ from distutils.core import setup
 setup(
   name = 'memetic',
   packages = ['memetic'], 
-  version = '1.0.1',
+  version = '1.0.3',
   description = 'An open source property graph schema and graph traverse language.',
   author = 'David Stocker',
   author_email = 'mrdave991@gmail.com',
   url = 'https://github.com/davidhstocker/Memetic', 
-  download_url = 'https://github.com/davidhstocker/Memetic/tarball/1.0.2', 
+  download_url = 'https://github.com/davidhstocker/Memetic/tarball/1.0.3', 
   keywords = ['graph', 'propertygraph', 'property graph', 'graphdatabase', 'graph database', 'schema'],
   classifiers = [],
 )
